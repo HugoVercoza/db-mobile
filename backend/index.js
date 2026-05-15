8e1a5dea591ebafaa4e2439c39a745ecb548bf29
Hugo Vercoza <hugotvercoza@gmail.com>
2026-05-15 14:23:05 -0300

    Implementando as rotas de busca e de limpeza do banco (Pinecone v4)

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const { Pinecone } = require('@pinecone-database/pinecone');
 const { pipeline, mean_pooling } = require('@xenova/transformers');
 const fs = require('fs');
 const { normalize } = require('path');
+const { get } = require('http');
 
 const app = express();
 app.use(express.json());
@@ -39,7 +40,11 @@ app.post('/upload-produtos', async (req,res) => {
     const listaDeEnvio = [];
 
     for (const produto of produtos){
-      const output = await gerarVetores(produto.descricao, {
+
+      //melhorando a qualidade de busca da IA
+      const textoVetor = `Produto: ${produto.nome}. Categoria: ${produto.categoria}. Descrição: ${produto.descricao}`
+
+      const output = await gerarVetores(textoVetor, {
         pooling: "mean",
         normalize: true
       });
@@ -66,6 +71,45 @@ app.post('/upload-produtos', async (req,res) => {
   } catch (error) {
     console.error("Erro ao concluir o processo", error);
     res.status(500).json({error: error.message});
+  }
+});
+
+// Rota de busca
+app.post('/buscar', async (req, res) => {
+  try {
+    const { busca } = req.body; // o que digitou
+    const gerarVetores = await getEmbedder();
+
+    // transforma o que digitou em vetor
+    const output = await gerarVetores(busca, {
+      pooling: 'mean',
+      normalize: true
+    });
+    const vetorBusca = Array.from(output.data);
+
+    // busca no banco os 5 mais parecidos (topK: 5)
+    const resultado = await index.query({
+      vector: vetorBusca,
+      topK: 5,
+      includeMetadata: true // metadata onde tem o nome e preço
+    });
+
+    res.json(resultado.matches);
+  } catch (error) {
+    console.error("Erro na busca: ", error);
+    res.status(500).json({error: error.message});
+  }
+});
+
+//Rota para limpar o banco
+app.delete('/limpar-banco', async (req, res) => {
+  try {
+    // Apaga os vetores do index
+    await index.deleteAll();
+    
+    res.json({ message: "Banco de dados limpo" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
